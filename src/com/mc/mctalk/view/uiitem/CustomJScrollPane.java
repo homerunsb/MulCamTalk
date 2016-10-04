974968424085b0364e6ab25f4aa66b3ea06ab8f0
homerunsb <homerunsb@gmail.com>
2016-10-04 10:45:00 +0900

    스크롤바 모양 변경
    굵기 및 보더 설정

--- src/com/mc/mctalk/view/uiitem/CustomJScrollPane.java
+++ src/com/mc/mctalk/view/uiitem/CustomJScrollPane.java
@@ -1,21 +1,26 @@
 package com.mc.mctalk.view.uiitem;
 
 import javax.swing.JScrollPane;
+import javax.swing.border.Border;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import javax.swing.*;
 import javax.swing.plaf.metal.MetalScrollBarUI;
 
 public class CustomJScrollPane extends JScrollPane {
-
-	/** @see http://stackoverflow.com/a/12270067/230513 */
-
 	public CustomJScrollPane() {
 		super();
 	}
 	public CustomJScrollPane(JList jlist) {
 		super(jlist);
 		JScrollBar sb = this.getVerticalScrollBar();
+		
+		//스크롤바 모양 설정 (굵기, 보더)
+		sb.setPreferredSize(new Dimension(15, 0));
+	    Border brd = BorderFactory.createMatteBorder(1, 3, 1, 3, new Color(238, 238, 238));
+		sb.setBorder(brd);
+		
 		sb.setUI(new MyScrollbarUI());
 	}
 
@@ -23,17 +28,15 @@ public class CustomJScrollPane extends JScrollPane {
 
 		private Image imageThumb, imageTrack;
 		private JButton b = new JButton() {
-
 			@Override
 			public Dimension getPreferredSize() {
 				return new Dimension(0, 0);
 			}
-
 		};
 
 		MyScrollbarUI() {
-			imageThumb = FauxImage.create(5, 5, Color.lightGray);
-			imageTrack = FauxImage.create(5, 5, new Color(238, 238, 238));
+			imageThumb = FauxImage.create(1, 1, Color.lightGray);
+			imageTrack = FauxImage.create(1, 1, new Color(238, 238, 238));
 		}
 
 		@Override
@@ -59,7 +62,6 @@ public class CustomJScrollPane extends JScrollPane {
 	}
 
 	private static class FauxImage {
-
 		static public Image create(int w, int h, Color c) {
 			BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 			Graphics2D g2d = bi.createGraphics();
